8319d480dcd2780e1676c307ebece7b72342d704
crispin <h.c.shin.dev09@gmail.com>
2024-12-28 14:40:44 +0900

    project init

---
+++ src/main/kotlin/com/inner/circle/pay200/Pay200Application.kt
@@ -0,0 +1,11 @@
+package com.inner.circle.pay200
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Pay200Application
+
+fun main(args: Array<String>) {
+    runApplication<Pay200Application>(*args)
+}
---
+++ src/test/kotlin/com/inner/circle/pay200/Pay200ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.inner.circle.pay200
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Pay200ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
