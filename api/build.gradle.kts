748934603f3842c9661745e59d2524d570d8f6b7
crispin <shoy1415@gmail.com>
2025-01-04 13:27:08 +0900

    feat: api 모듈 생성

---
+++ api/build.gradle.kts
@@ -0,0 +1,28 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    id("kotlin-jvm")
+    alias(libs.plugins.springBoot)
+    alias(libs.plugins.kotlinPluginSpring)
+    alias(libs.plugins.springDependencyManagement)
+}
+
+dependencies {
+    libs.bundles.boms
+        .get()
+        .forEach { implementation(platform(it)) }
+    implementation(projects.core)
+    implementation(libs.bundles.springBoot)
+    testImplementation(libs.bundles.kotlinTestSpring)
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+    testLogging {
+        events(
+            TestLogEvent.FAILED,
+            TestLogEvent.PASSED,
+            TestLogEvent.SKIPPED
+        )
+    }
+}
---
+++ core/src/test/kotlin/com/inner/circle/core/CalculatorTest.kt
@@ -0,0 +1,4 @@
+package com.inner.circle.core
+
+import org.junit.jupiter.api.Assertions.*
+ class CalculatorTest
