f7bbc766385799416e761aa7268b0abe9b714436
crispin <shoy1415@gmail.com>
2025-01-04 13:21:30 +0900

    feat: 멀티 모듈 구성을 위한 settings.gradle 파일 수정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,23 @@
 rootProject.name = "pay200"
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+    }
+}
+
+pluginManagement {
+    includeBuild("build-logic")
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+include(
+    ":core",
+    ":api"
+)
