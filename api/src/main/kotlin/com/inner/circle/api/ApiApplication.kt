42d75c810652ef4f5164e8eb463589215ea1adac
crispin <h.c.shin.dev09@gmail.com>
2025-01-07 19:35:05 +0900

    delete: jvm 메모리 설정 제거

--- api/src/main/kotlin/com/inner/circle/api/ApiApplication.kt
+++ api/src/main/kotlin/com/inner/circle/api/ApiApplication.kt
@@ -3,7 +3,9 @@ package com.inner.circle.api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = ["com.inner.circle.core", "com.inner.circle.infra"]
+)
 class ApiApplication
 
 fun main(args: Array<String>) {
