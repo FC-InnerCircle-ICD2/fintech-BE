76a7301b89386397c17380c616049882130dc010
gentledot <gentledot.wp@gmail.com>
2025-01-09 22:27:36 +0900

    feat(#34): swagger 간단 구성
    - basic auth 구성

--- api/src/main/kotlin/com/inner/circle/api/config/SwaggerConfig.kt
+++ api/src/main/kotlin/com/inner/circle/api/config/SwaggerConfig.kt
@@ -2,11 +2,15 @@ package com.inner.circle.api.config
 
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SwaggerConfig {
+    val securitySchemeName = "basicAuth"
+
     @Bean
     fun customOpenAPI(): OpenAPI =
         OpenAPI()
@@ -16,4 +20,16 @@ class SwaggerConfig {
                     .version("1.0")
                     .description("PG 결제 프로젝트")
             )
+            .addSecurityItem(SecurityRequirement().addList(securitySchemeName))
+            .components(
+                io.swagger.v3.oas.models.Components()
+                    .addSecuritySchemes(
+                        securitySchemeName,
+                        SecurityScheme()
+                            .name(securitySchemeName)
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("basic")
+                    )
+            )
+
 }
