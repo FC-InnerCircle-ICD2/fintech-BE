daea13a7b66de4ae7d3a0d40821f2d90af85aa7c
behappyleee <72903623+behappyleee@users.noreply.github.com>
2025-01-11 22:59:45 +0900

    feat: url 공통화 커밋

---
+++ api/src/main/kotlin/com/inner/circle/api/payment/controller/PaymentAuthenticationController.kt
@@ -0,0 +1,18 @@
+package com.inner.circle.api.payment.controller
+
+import com.inner.circle.api.util.UrlConstants
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(UrlConstants.PAYMENT_V1_API_URL_PREFIX)
+class PaymentAuthenticationController {
+
+    @PostMapping("{orderId}/proceed")
+    fun proceedAuthentication(
+        @PathVariable(name = "orderId") orderId: String,
+    ) {}
+}
+
---
+++ api/src/main/kotlin/com/inner/circle/api/util/UrlConstants.kt
@@ -0,0 +1,5 @@
+package com.inner.circle.api.util
+
+object UrlConstants {
+    const val PAYMENT_V1_API_URL_PREFIX = "api/v1/payments/"
+}
