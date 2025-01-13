2255dc2e6a9defd0b3f662669b85fb00c175a81b
raemin.kang <raemin.kang@navercorp.com>
2025-01-13 23:21:50 +0900

    feat: Postsql local docker 환경 구축
    - testcontainer를 활용한 local postgresql 구축
    
    #57

---
+++ api-backoffice/src/test/kotlin/com/inner/circle/apibackoffice/TestApiBackofficeApplication.kt
@@ -0,0 +1,13 @@
+import com.inner.circle.apibackoffice.ApiBackofficeApplication
+import com.inner.circle.apibackoffice.config.PostgreSqlTestContainerConfiguration
+import org.springframework.boot.runApplication
+
+object TestApiBackofficeApplication {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        runApplication<ApiBackofficeApplication>(*args) {
+            addInitializers(PostgreSqlTestContainerConfiguration())
+        }
+    }
+}
