8e9d756a8c6d24f10bb59d931a4fb298c0f2cb8f
Raemin Kang <29060626+Raemerrr@users.noreply.github.com>
2025-01-14 21:39:20 +0900

    feat: Postsql local docker 환경 구축 (#58)
    - testcontainer를 활용한 local postgresql 구축
    
    #57

--- api-backoffice/src/main/kotlin/com/inner/circle/apibackoffice/ApiBackofficeApplication.kt
+++ api-backoffice/src/main/kotlin/com/inner/circle/apibackoffice/ApiBackofficeApplication.kt
@@ -3,7 +3,13 @@ package com.inner.circle.apibackoffice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = [
+        "com.inner.circle.apibackoffice",
+        "com.inner.circle.corebackoffice",
+        "com.inner.circle.infrabackoffice"
+    ]
+)
 class ApiBackofficeApplication
 
 fun main(args: Array<String>) {
