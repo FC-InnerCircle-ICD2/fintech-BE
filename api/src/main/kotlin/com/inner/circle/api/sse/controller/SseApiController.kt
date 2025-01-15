c3843fe5dc080e741ef7e5ffe7a0a2ed14723221
InvestLee <dlwlgud216@nate.com>
2025-01-15 20:55:18 +0900

    주석 위치 변경

--- api/src/main/kotlin/com/inner/circle/api/sse/controller/SseApiController.kt
+++ api/src/main/kotlin/com/inner/circle/api/sse/controller/SseApiController.kt
@@ -1,16 +1,16 @@
 package com.inner.circle.api.sse.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.inner.circle.api.sse.connection.SseConnectionPool
 import com.inner.circle.api.sse.connection.SseConnection
+import com.inner.circle.api.sse.connection.SseConnectionPool
+import io.swagger.v3.oas.annotations.Parameter
 import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter
-import org.springframework.security.core.annotation.AuthenticationPrincipal
-import io.swagger.v3.oas.annotations.Parameter
 
 @RestController
 @RequestMapping("/api/sse")
@@ -18,7 +18,6 @@ class SseApiController(
     private val sseConnectionPool: SseConnectionPool,
     private val objectMapper: ObjectMapper
 ) {
-
     companion object {
         private val log = LoggerFactory.getLogger(SseApiController::class.java)
     }
@@ -26,15 +25,17 @@ class SseApiController(
     @GetMapping(path = ["/connect"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
     fun connect(
         @Parameter(hidden = true)
-        @AuthenticationPrincipal session: String //세션 객체로 변경 예정
+        // 세션 객체로 변경 예정
+        @AuthenticationPrincipal session: String
     ): ResponseBodyEmitter {
         log.info("login user {}", session)
 
-        val sseConnection = SseConnection.connect(
-            session,
-            sseConnectionPool,
-            objectMapper
-        )
+        val sseConnection =
+            SseConnection.connect(
+                session,
+                sseConnectionPool,
+                objectMapper
+            )
 
         sseConnectionPool.addSession(sseConnection.uniqueKey, sseConnection)
 
@@ -44,7 +45,8 @@ class SseApiController(
     @GetMapping("/push-event")
     fun pushEvent(
         @Parameter(hidden = true)
-        @AuthenticationPrincipal session: String //세션 객체로 변경 예정
+        // 세션 객체로 변경 예정
+        @AuthenticationPrincipal session: String
     ) {
         val sseConnection = sseConnectionPool.getSession(session)
 
