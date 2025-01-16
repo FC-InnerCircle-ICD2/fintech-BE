43fa987c7a4deae16a40010152bed36863009964
youable <youable.framios@gmail.com>
2025-01-16 19:44:40 +0900

    Add: MerchantEntity 생성 (#26)

---
+++ infra-backoffice/src/main/kotlin/com/inner/circle/infrabackoffice/structure/repository/entity/MerchantEntity.kt
@@ -0,0 +1,19 @@
+package com.inner.circle.infrabackoffice.structure.repository.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "merchant")
+data class MerchantEntity(
+    @Id
+    val id: String,
+    @Column(nullable = false, unique = true)
+    val mid: String,
+    @Column(nullable = false, unique = true)
+    val token: String,
+    @Column(nullable = false)
+    val name: String
+) : BaseEntity()
