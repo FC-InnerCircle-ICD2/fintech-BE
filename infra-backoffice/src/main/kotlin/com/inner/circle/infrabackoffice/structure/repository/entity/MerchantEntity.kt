d0c2ba5ce7335d3c87e42daa54c1872eaec07afd
youable <youable.framios@gmail.com>
2025-01-18 01:35:37 +0900

    Fix: MerchantEntity 수정 (#26)
    - JPA에서 필요로 하는 기본 생성자 추가
    - token 컬럼 설명 추가
    - 추후 apply plugin: kotlin-jpa를 추가하는 방향으로 수정

--- infra-backoffice/src/main/kotlin/com/inner/circle/infrabackoffice/structure/repository/entity/MerchantEntity.kt
+++ infra-backoffice/src/main/kotlin/com/inner/circle/infrabackoffice/structure/repository/entity/MerchantEntity.kt
@@ -4,6 +4,7 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import org.hibernate.annotations.Comment
 
 @Entity
 @Table(name = "merchant")
@@ -12,8 +13,11 @@ data class MerchantEntity(
     val id: String,
     @Column(nullable = false, unique = true)
     val mid: String,
+    @Comment("client_secret")
     @Column(nullable = false, unique = true)
-    val token: String,
+    var token: String,
     @Column(nullable = false)
     val name: String
-) : BaseEntity()
+) : BaseEntity() {
+    constructor() : this("", "", "", "")
+}
