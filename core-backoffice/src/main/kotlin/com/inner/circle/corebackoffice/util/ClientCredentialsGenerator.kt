b523d186c9e62bc2ba2e05987f962ec849adf3f8
youable <youable.framios@gmail.com>
2025-01-18 01:32:18 +0900

    Fix: ClientCredentialsGenerator 토큰 생성 수정 (#26)

--- core-backoffice/src/main/kotlin/com/inner/circle/corebackoffice/util/ClientCredentialsGenerator.kt
+++ core-backoffice/src/main/kotlin/com/inner/circle/corebackoffice/util/ClientCredentialsGenerator.kt
@@ -1,4 +1,21 @@
 package com.inner.circle.corebackoffice.util
 
-class ClientCredentialsGenerator {
+import java.security.SecureRandom
+import java.util.*
+
+object ClientCredentialsGenerator {
+    private val secureRandom = SecureRandom()
+    private val base64Encoder = Base64.getUrlEncoder()
+
+    fun generateClientId(): String {
+        val randomBytes = ByteArray(24)
+        secureRandom.nextBytes(randomBytes)
+        return base64Encoder.encodeToString(randomBytes)
+    }
+
+    fun generateClientSecret(): String {
+        val randomBytes = ByteArray(32)
+        secureRandom.nextBytes(randomBytes)
+        return base64Encoder.encodeToString(randomBytes)
+    }
 }
