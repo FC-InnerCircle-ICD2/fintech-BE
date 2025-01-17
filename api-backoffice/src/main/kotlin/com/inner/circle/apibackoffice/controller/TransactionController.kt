eeb3d199ecbeda69172d166da7841f19d81378fe
raemerrr <foals18@gmail.com>
2025-01-17 21:51:43 +0900

    feat(api): Backoffice Payment 조회 로직 추가
    #73

--- api-backoffice/src/main/kotlin/com/inner/circle/apibackoffice/controller/TransactionController.kt
+++ api-backoffice/src/main/kotlin/com/inner/circle/apibackoffice/controller/TransactionController.kt
@@ -1,14 +1,12 @@
-package com.inner.circle.apibackoffice.transaction.controller
+package com.inner.circle.apibackoffice.controller
 
+import com.inner.circle.apibackoffice.controller.dto.TransactionDto
+import com.inner.circle.apibackoffice.controller.dto.TransactionsDto
 import com.inner.circle.apibackoffice.exception.BackofficeResponse
-import com.inner.circle.apibackoffice.transaction.controller.dto.TransactionDto
-import com.inner.circle.apibackoffice.transaction.controller.dto.TransactionStatus
-import com.inner.circle.apibackoffice.transaction.controller.dto.TransactionsDto
 import com.inner.circle.corebackoffice.usecase.ResponseTransactionUseCase
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import java.util.UUID
-import kotlinx.datetime.toJavaLocalDateTime
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -33,19 +31,8 @@ class TransactionController(
             TransactionsDto(
                 responseTransactionUseCase
                     .getTransactions(request)
-                    .map { transaction ->
-                        TransactionDto(
-                            transaction.id,
-                            transaction.paymentId,
-                            transaction.amount,
-                            TransactionStatus.of(transaction.status),
-                            transaction.reason,
-                            transaction.requestedAt.toJavaLocalDateTime(),
-                            transaction.completedAt.toJavaLocalDateTime(),
-                            transaction.createdAt.toJavaLocalDateTime(),
-                            transaction.updatedAt.toJavaLocalDateTime()
-                        )
-                    }.toList()
+                    .map { transaction -> TransactionDto.of(transaction) }
+                    .toList()
             )
         )
     }
