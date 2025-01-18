8c0eba24a9487deb0328e7c6182b90261024ac19
gentledot <gentledot.wp@gmail.com>
2025-01-18 11:47:13 +0900

    #36 : paymentRequest 생성 과정 구현 (4)
    - formatter 수행

--- core/src/main/kotlin/com/inner/circle/core/structure/dto/PaymentClaimResponse.kt
+++ core/src/main/kotlin/com/inner/circle/core/structure/dto/PaymentClaimResponse.kt
@@ -2,16 +2,19 @@ package com.inner.circle.core.structure.dto
 
 import java.time.LocalDateTime
 
-
 data class PaymentClaimResponse(
     val token: String,
-    val expiredAt: LocalDateTime,
+    val expiredAt: LocalDateTime
 ) {
     companion object {
-        fun of(intpuToken: String, tokenExpiredAt: LocalDateTime): PaymentClaimResponse =
-            PaymentClaimResponse(intpuToken, tokenExpiredAt)
+        fun of(
+            intpuToken: String,
+            tokenExpiredAt: LocalDateTime
+        ): PaymentClaimResponse = PaymentClaimResponse(intpuToken, tokenExpiredAt)
 
-        fun testOne(intpuToken: String, tokenExpiredAt: LocalDateTime): PaymentClaimResponse =
-            PaymentClaimResponse("test", LocalDateTime.now())
+        fun testOne(
+            intpuToken: String,
+            tokenExpiredAt: LocalDateTime
+        ): PaymentClaimResponse = PaymentClaimResponse("test", LocalDateTime.now())
     }
 }
