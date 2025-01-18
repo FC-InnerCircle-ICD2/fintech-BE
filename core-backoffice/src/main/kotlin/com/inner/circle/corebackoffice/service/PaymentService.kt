567d1b13c529b716b7b4fdc255c19eeb9779ac63
raemerrr <foals18@gmail.com>
2025-01-18 14:12:46 +0900

    포멧팅 수정

--- core-backoffice/src/main/kotlin/com/inner/circle/corebackoffice/service/PaymentService.kt
+++ core-backoffice/src/main/kotlin/com/inner/circle/corebackoffice/service/PaymentService.kt
@@ -11,22 +11,22 @@ import org.springframework.stereotype.Service
 internal class PaymentService(
     private val getPaymentPort: GetPaymentPort
 ) : GetPaymentUseCase {
-    override fun getPaymentByPaymentKey(request: GetPaymentUseCase.Request): PaymentDto {
-        return getPaymentPort.getPaymentByPaymentKey(
-            GetPaymentPort.Request(
-                paymentKey = request.paymentKey
-            )
-        ).let {
-            PaymentDto(
-                id = it.id,
-                paymentKey = it.paymentKey,
-                currency = Currency.of(it.currency),
-                userId = it.userId,
-                merchantId = it.merchantId,
-                paymentType = PaymentType.of(it.paymentType),
-                createdAt = it.createdAt,
-                updatedAt = it.updatedAt
-            )
-        }
-    }
+    override fun getPaymentByPaymentKey(request: GetPaymentUseCase.Request): PaymentDto =
+        getPaymentPort
+            .getPaymentByPaymentKey(
+                GetPaymentPort.Request(
+                    paymentKey = request.paymentKey
+                )
+            ).let {
+                PaymentDto(
+                    id = it.id,
+                    paymentKey = it.paymentKey,
+                    currency = Currency.of(it.currency),
+                    userId = it.userId,
+                    merchantId = it.merchantId,
+                    paymentType = PaymentType.of(it.paymentType),
+                    createdAt = it.createdAt,
+                    updatedAt = it.updatedAt
+                )
+            }
 }
