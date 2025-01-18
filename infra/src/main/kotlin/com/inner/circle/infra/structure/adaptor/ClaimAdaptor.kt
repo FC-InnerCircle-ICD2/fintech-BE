f84210c00a17fc49e05263d547b6b776a5f4bcb4
gentledot <gentledot.wp@gmail.com>
2025-01-18 15:13:49 +0900

    #36 : paymentRequest 생성 과정 구현 (8)
    - PaymentClaimRepository 구성하여 paymentRequest 생성 로직 구성

--- infra/src/main/kotlin/com/inner/circle/infra/structure/adaptor/ClaimAdaptor.kt
+++ infra/src/main/kotlin/com/inner/circle/infra/structure/adaptor/ClaimAdaptor.kt
@@ -2,12 +2,12 @@ package com.inner.circle.infra.structure.adaptor
 
 import com.inner.circle.infra.structure.adaptor.dto.PaymentRequestDto
 import com.inner.circle.infra.structure.port.ClaimHandlingPort
-import com.inner.circle.infra.structure.repository.PaymentRequestRepository
+import com.inner.circle.infra.structure.repository.PaymentClaimRepository
 import org.springframework.stereotype.Component
 
 @Component
 internal class ClaimAdaptor(
-    private val repository: PaymentRequestRepository
+    private val repository: PaymentClaimRepository
 ) : ClaimHandlingPort {
     override fun generatePaymentRequest(paymentRequestData: PaymentRequestDto): PaymentRequestDto {
         val paymentRequest = paymentRequestData.toEntity()
