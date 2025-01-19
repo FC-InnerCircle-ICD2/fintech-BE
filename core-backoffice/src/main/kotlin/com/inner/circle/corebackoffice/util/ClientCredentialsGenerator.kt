1bfc59323b23c93e7e626258c56116d77a82aa10
youable <youable.framios@gmail.com>
2025-01-19 20:59:10 +0900

    Fix: 고유한 api key가 생성되도록 timestamp 추가 (#26)

--- core-backoffice/src/main/kotlin/com/inner/circle/corebackoffice/util/ClientCredentialsGenerator.kt
+++ core-backoffice/src/main/kotlin/com/inner/circle/corebackoffice/util/ClientCredentialsGenerator.kt
@@ -1,7 +1,8 @@
 package com.inner.circle.corebackoffice.util
 
+import java.security.MessageDigest
 import java.security.SecureRandom
-import java.util.*
+import java.util.Base64
 
 object ClientCredentialsGenerator {
     private val secureRandom = SecureRandom()
@@ -16,6 +17,10 @@ object ClientCredentialsGenerator {
     fun generateClientSecret(): String {
         val randomBytes = ByteArray(32)
         secureRandom.nextBytes(randomBytes)
-        return base64Encoder.encodeToString(randomBytes)
+
+        val timestamp = System.nanoTime().toString()
+        val input = randomBytes.joinToString("") + timestamp
+        val hash = MessageDigest.getInstance("SHA-256").digest(input.toByteArray())
+        return base64Encoder.encodeToString(hash)
     }
 }
