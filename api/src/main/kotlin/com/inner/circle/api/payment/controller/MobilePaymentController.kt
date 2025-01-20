cd8352e20789a11d664ca33e0cd97d7e712fa897
InvestLee <dlwlgud216@nate.com>
2025-01-20 20:33:55 +0900

    카드 인증 및 결제 확인 api 2차 반영 수정

--- api/src/main/kotlin/com/inner/circle/api/payment/controller/MobilePaymentController.kt
+++ api/src/main/kotlin/com/inner/circle/api/payment/controller/MobilePaymentController.kt
@@ -6,13 +6,10 @@ import com.inner.circle.api.payment.controller.dto.UserCardDto
 import com.inner.circle.core.structure.usecase.MobilePaymentUseCase
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @Tag(name = "MobilePayment", description = "MobilePayment API")
@@ -21,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController
 class MobilePaymentController(
     private val mobilePaymentUseCase: MobilePaymentUseCase
 ) {
-
     @Operation(summary = "모바일 간편 결제 요청")
     @PostMapping("/proceed/simple/{order_id}")
     fun proceedMobilePayment(
