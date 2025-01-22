5110c7827abdad696cddcd316d05b2310191cefe
Raemin Kang <29060626+Raemerrr@users.noreply.github.com>
2025-01-22 21:19:45 +0900

    feat: HealthCheck 로직 추가 (#116)

--- api-backoffice/src/main/kotlin/com/inner/circle/apibackoffice/controller/HealthCheckController.kt
+++ api-backoffice/src/main/kotlin/com/inner/circle/apibackoffice/controller/HealthCheckController.kt
@@ -1,10 +1,9 @@
-package com.inner.circle.api.healthcheck.controller
+package com.inner.circle.apibackoffice.controller
 
-import com.inner.circle.api.controller.PaymentV1Api
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
 
-@PaymentV1Api
+@BackofficeV1Api
 class HealthCheckController {
     @GetMapping("/health-check")
     fun check(
