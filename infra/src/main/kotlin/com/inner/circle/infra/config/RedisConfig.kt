805b27c0e32bb2696c30d224dd688561bf112e9c
gentledot <gentledot.wp@gmail.com>
2025-02-01 23:10:22 +0900

    Infra/add elastic cache (#134)
    * feat(infra-reddis): elastic-cache 연결 설정을 위한 embeddedRedis 제거
    
    * feat(infra-reddis): elastic-cache 연결 설정
    
    - redis_host 추가 구성
    - ssl true 처리
    
    * feat(infra-reddis): elastic-cache 연결 설정
    
    - LettuceConnectionFactory 설정
    
    * feat(infra-reddis): elastic-cache 연결 설정
    
    - LettuceConnectionFactory 설정
    
    * feat(infra-reddis): elastic-cache 연결 설정
    
    - ddl_auto: none 설정

--- infra/src/main/kotlin/com/inner/circle/infra/config/RedisConfig.kt
+++ infra/src/main/kotlin/com/inner/circle/infra/config/RedisConfig.kt
@@ -4,6 +4,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
@@ -15,8 +17,19 @@ class RedisConfig {
     lateinit var host: String
     var port: Int = 6379
 
+    // https://redis.github.io/lettuce/#ssl
     @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory = LettuceConnectionFactory(host, port)
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val redisConfiguration = RedisStandaloneConfiguration(host, port)
+        val lettuceClientConfiguration =
+            LettuceClientConfiguration
+                .builder()
+                .useSsl()
+                .disablePeerVerification()
+                .build()
+
+        return LettuceConnectionFactory(redisConfiguration, lettuceClientConfiguration)
+    }
 
     @Bean
     fun redisTemplate(): StringRedisTemplate {
