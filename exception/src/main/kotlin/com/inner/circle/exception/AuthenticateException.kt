90c014ed0d4aa5a11c4eaf3ac7ffaaa35b086f2b
InvestLee <dlwlgud216@nate.com>
2025-02-03 22:04:52 +0900

    paymentEntity, TransactionEntity 컬럼 수정 및 tsid 도입에 따른 로직 수정, userId를 accountId로 수정

--- exception/src/main/kotlin/com/inner/circle/exception/AuthenticateException.kt
+++ exception/src/main/kotlin/com/inner/circle/exception/AuthenticateException.kt
@@ -11,7 +11,8 @@ sealed class AuthenticateException(
     ) : PaymentException(HttpStatus.BAD_REQUEST, message, cause)
 
     data class CardNotFoundException(
-        override val message: String = "Simple payment is not possible because card information does not exist.",
+        override val message: String
+        = "Simple payment is not possible because card information does not exist.",
         override val cause: Throwable? = null
     ) : PaymentException(HttpStatus.BAD_REQUEST, message, cause)
 }
