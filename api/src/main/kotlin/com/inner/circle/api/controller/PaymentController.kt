e5a0412d466834764db11f1910ddcc79ac21a823
Raemin Kang <29060626+Raemerrr@users.noreply.github.com>
2025-02-03 21:45:16 +0900

    refactor: payment 관련 uri 수정 (#141)
    * refactor: payment 관련 uri 수정
    
    #139

--- api/src/main/kotlin/com/inner/circle/api/controller/PaymentController.kt
+++ api/src/main/kotlin/com/inner/circle/api/controller/PaymentController.kt
@@ -1,11 +1,15 @@
 package com.inner.circle.api.controller
 
 import com.inner.circle.api.controller.dto.ConfirmPaymentDto
+import com.inner.circle.api.controller.dto.PaymentApproveDto
 import com.inner.circle.api.controller.dto.PaymentResponse
 import com.inner.circle.api.controller.request.ConfirmPaymentRequest
 import com.inner.circle.api.controller.request.ConfirmSimplePaymentRequest
+import com.inner.circle.api.controller.request.PaymentApproveRequest
 import com.inner.circle.core.usecase.ConfirmPaymentUseCase
 import com.inner.circle.core.usecase.ConfirmSimplePaymentUseCase
+import com.inner.circle.core.usecase.PaymentClaimUseCase
+import com.inner.circle.core.usecase.SavePaymentApproveUseCase
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,13 +17,47 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
-@Tag(name = "ConfirmPayment", description = "ConfirmPayment API")
+@Tag(name = "Payments", description = "결제 관련 API")
 @PaymentV1Api
-class ConfirmPaymentController(
-    private val confirmPaymentUseCase: ConfirmPaymentUseCase
+class PaymentController(
+    private val confirmPaymentUseCase: ConfirmPaymentUseCase,
+    private val claimUseCase: PaymentClaimUseCase,
+    private val savePaymentApproveService: SavePaymentApproveUseCase
 ) {
-    @Operation(summary = "간편 결제 확인")
-    @PostMapping("/proceed/simple")
+    @Operation(summary = "결제 요청")
+    @PostMapping("/payments")
+    fun createPayment(
+        @RequestBody request: PaymentClaimUseCase.PaymentClaimRequest
+    ): PaymentResponse<PaymentClaimUseCase.PaymentClaimResponse> {
+        val merchantId = "tempMerchantId" // @AuthenticationPrincipal
+        val response = claimUseCase.createPayment(request, merchantId)
+        return PaymentResponse.ok(response)
+    }
+
+    @Operation(summary = "결제 승인")
+    @PostMapping("/payments/confirm")
+    fun confirmPayment(
+        @RequestBody paymentApproveRequest: PaymentApproveRequest
+    ): PaymentResponse<PaymentApproveDto> =
+        PaymentResponse.ok(
+            savePaymentApproveService
+                .saveApprove(
+                    SavePaymentApproveUseCase.Request(
+                        paymentKey = paymentApproveRequest.paymentKey,
+                        orderId = paymentApproveRequest.orderId,
+                        amount = paymentApproveRequest.amount
+                    )
+                ).let {
+                    PaymentApproveDto(
+                        orderId = it.orderId,
+                        paymentKey = it.paymentKey,
+                        amount = it.amount
+                    )
+                }
+        )
+
+    @Operation(summary = "간편 결제 인증")
+    @PostMapping("/payments/authentication/simple")
     fun proceedPaymentConfirm(
         @RequestBody confirmSimplePaymentRequest: ConfirmSimplePaymentRequest
     ): PaymentResponse<ConfirmPaymentDto> {
@@ -37,8 +75,8 @@ class ConfirmPaymentController(
         return response
     }
 
-    @Operation(summary = "결제 확인")
-    @PostMapping("/proceed")
+    @Operation(summary = "일반 결제 인증")
+    @PostMapping("/payments/authentication")
     fun proceedPaymentConfirm(
         @RequestBody confirmPaymentRequest: ConfirmPaymentRequest
     ): PaymentResponse<ConfirmPaymentDto> {
@@ -59,9 +97,8 @@ class ConfirmPaymentController(
         return response
     }
 
-    // Frontend 분들과 협의 필요
     @Operation(summary = "결제 취소")
-    @GetMapping("/cancel/{order_id}")
+    @GetMapping("/payments/orders/{order_id}/cancel")
     fun cancelPaymentConfirm(
         @PathVariable("order_id") orderId: String
     ): PaymentResponse<String> {
