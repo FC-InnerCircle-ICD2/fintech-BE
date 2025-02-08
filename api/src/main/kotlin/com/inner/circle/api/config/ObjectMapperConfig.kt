81d8d5bb0fae5ee38a323b4bfe23e6836ecc3b3a
behappyleee <72903623+behappyleee@users.noreply.github.com>
2025-02-08 09:10:11 +0900

    feat: add mapper config

--- api/src/main/kotlin/com/inner/circle/api/config/ObjectMapperConfig.kt
+++ api/src/main/kotlin/com/inner/circle/api/config/ObjectMapperConfig.kt
@@ -18,7 +18,7 @@ class ObjectMapperConfig {
             .registerModule(kotlinModule())
             .registerModule(JavaTimeModule())
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
             .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
             .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
 }
