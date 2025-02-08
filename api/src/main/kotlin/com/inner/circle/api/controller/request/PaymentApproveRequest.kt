35b0e06f03064c99228bb0371e89ca5d7c478644
InvestLee <dlwlgud216@nate.com>
2025-02-08 15:41:30 +0900

    ObjectMappeer변경에 따른 Request 수정

--- api/src/main/kotlin/com/inner/circle/api/controller/request/PaymentApproveRequest.kt
+++ api/src/main/kotlin/com/inner/circle/api/controller/request/PaymentApproveRequest.kt
@@ -1,13 +1,9 @@
 package com.inner.circle.api.controller.request
 
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
 import java.math.BigDecimal
 
-data class PaymentApproveRequest
-    @JsonCreator
-    constructor(
-        @JsonProperty("orderId") val orderId: String,
-        @JsonProperty("paymentKey") val paymentKey: String,
-        @JsonProperty("amount") val amount: BigDecimal
+data class PaymentApproveRequest(
+        val orderId: String,
+        val paymentKey: String,
+        val amount: BigDecimal
     )
