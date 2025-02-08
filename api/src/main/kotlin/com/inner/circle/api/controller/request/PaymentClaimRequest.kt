71240676cf16482b3041526cb46ab55158dc0502
gentledot <gentledot.wp@gmail.com>
2025-02-08 16:26:27 +0900

    fix(36): 추가적으로 @JsonProperty 의 제거 처리

--- api/src/main/kotlin/com/inner/circle/api/controller/request/PaymentClaimRequest.kt
+++ api/src/main/kotlin/com/inner/circle/api/controller/request/PaymentClaimRequest.kt
@@ -1,13 +1,9 @@
 package com.inner.circle.api.controller.request
 
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
 import java.math.BigDecimal
 
-data class PaymentClaimRequest
-    @JsonCreator
-    constructor(
-        @JsonProperty("amount") val amount: BigDecimal,
-        @JsonProperty("orderId") val orderId: String,
-        @JsonProperty("orderName") val orderName: String
-    )
+data class PaymentClaimRequest(
+    val amount: BigDecimal,
+    val orderId: String,
+    val orderName: String
+)
--- core/src/main/kotlin/com/inner/circle/core/usecase/PaymentClaimUseCase.kt
+++ core/src/main/kotlin/com/inner/circle/core/usecase/PaymentClaimUseCase.kt
@@ -1,32 +1,26 @@
 package com.inner.circle.core.usecase
 
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
 import java.math.BigDecimal
 import java.time.LocalDateTime
 
 fun interface PaymentClaimUseCase {
-    data class ClaimRequest
-        @JsonCreator
-        constructor(
-            @JsonProperty("amount") val amount: BigDecimal,
-            @JsonProperty("orderId") val orderId: String,
-            @JsonProperty("orderName") val orderName: String
-        )
+    data class ClaimRequest(
+        val amount: BigDecimal,
+        val orderId: String,
+        val orderName: String
+    )
 
-    data class PaymentClaimResponse
-        @JsonCreator
-        constructor(
-            @JsonProperty("token") val token: String,
-            @JsonProperty("expiredAt") val expiredAt: LocalDateTime
-        ) {
-            companion object {
-                fun of(
-                    inputToken: String,
-                    tokenExpiredAt: LocalDateTime
-                ): PaymentClaimResponse = PaymentClaimResponse(inputToken, tokenExpiredAt)
-            }
+    data class PaymentClaimResponse(
+        val token: String,
+        val expiredAt: LocalDateTime
+    ) {
+        companion object {
+            fun of(
+                inputToken: String,
+                tokenExpiredAt: LocalDateTime
+            ): PaymentClaimResponse = PaymentClaimResponse(inputToken, tokenExpiredAt)
         }
+    }
 
     fun createPayment(
         request: ClaimRequest,
