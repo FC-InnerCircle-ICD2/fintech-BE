c1148438c598d23bd802bd62c6700baf06251c12
behappyleee <72903623+behappyleee@users.noreply.github.com>
2025-02-09 15:32:37 +0900

    feat: 회원 Account 테이블 엔티티 수정

--- infra/src/main/kotlin/com/inner/circle/infra/repository/entity/AccountEntity.kt
+++ infra/src/main/kotlin/com/inner/circle/infra/repository/entity/AccountEntity.kt
@@ -1,15 +1,25 @@
 package com.inner.circle.infra.repository.entity
 
 import jakarta.persistence.Column
+import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 import java.util.UUID
 
+@Entity
+@Table(name = "account")
 data class AccountEntity(
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    var id: UUID? = null,
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
     @Column(nullable = false, unique = true)
-    val name: String
+    val uuid: UUID,
+    @Column(nullable = false, unique = true, length = 255)
+    val email: String,
+    @Column(nullable = false, unique = false, length = 255)
+    val password: String,
+    @Column(nullable = false, unique = false, length = 1)
+    val status: Int,
 ) : BaseEntity()
