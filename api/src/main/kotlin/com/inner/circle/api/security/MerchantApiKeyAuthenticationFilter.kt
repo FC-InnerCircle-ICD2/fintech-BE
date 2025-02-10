50d7e94d8743ddcebdd8130f7342ba97863db279
youable <115457023+youabledev@users.noreply.github.com>
2025-02-10 23:38:57 +0900

    feat: 상점 인증 Spring Security 구현 (#181)
    * feat: #180 상점 인증용 Spring Security 설정
    
    * fix: #180 lint 이슈 수정
    
    * fix: #180 lint 이슈 수정
    
    * refactor: #180 infra에서 Security 제거
    
    * refactor: #180 core와 api 간 의존성 사용, 에러 가능성 있는 코드 보완
    
    * fix: #180 와일드 카드 자동 변경 수정
    
    * add: #180 test용 mock merchantUserDetails 추가
    
    * refactor: #180 security에서 발생하는 Exception UserAuthenticationException으로 정의
    
    * fix: lint
    
    * fix: #180 Merchant role 통일
    
    Issue: #180

---
+++ api/src/main/kotlin/com/inner/circle/api/security/MerchantApiKeyAuthenticationFilter.kt
@@ -0,0 +1,70 @@
+package com.inner.circle.api.security
+
+import com.inner.circle.core.security.MerchantApiKeyProvider
+import com.inner.circle.exception.UserAuthenticationException
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpHeaders
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+
+class MerchantApiKeyAuthenticationFilter(
+    private val provider: MerchantApiKeyProvider
+) : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val authHeader =
+            request.getHeader(HttpHeaders.AUTHORIZATION)
+                ?: throw UserAuthenticationException.UnauthorizedException(
+                    "Missing Authorization header"
+                )
+
+        val apiKey = resolveApiKey(authHeader)
+
+        val authentication = provider.getAuthentication(apiKey)
+        SecurityContextHolder.getContext().authentication = authentication
+
+        filterChain.doFilter(request, response)
+    }
+
+    private fun resolveApiKey(authHeader: String): String {
+        authenticateWithBasicAuth(authHeader = authHeader)
+
+        val authorizationInfo = authHeader.split(" ")
+        if (authorizationInfo.size != 2 || authorizationInfo[0] != BASIC_AUTH_TOKEN_PREFIX) {
+            throw UserAuthenticationException.UnauthorizedException()
+        }
+
+        val encodedApiKey = authorizationInfo[1]
+        val decodedApiKey =
+            try {
+                String(
+                    java.util.Base64
+                        .getDecoder()
+                        .decode(encodedApiKey)
+                )
+            } catch (e: IllegalArgumentException) {
+                throw UserAuthenticationException.UnauthorizedException()
+            }
+
+        if (!decodedApiKey.contains(":")) {
+            throw UserAuthenticationException.UnauthorizedException()
+        }
+
+        return decodedApiKey.substringBefore(":")
+    }
+
+    private fun authenticateWithBasicAuth(authHeader: String?) {
+        if (authHeader == null || !authHeader.startsWith(prefix = BASIC_AUTH_TOKEN_PREFIX)) {
+            throw UserAuthenticationException.UnauthorizedException()
+        }
+    }
+
+    companion object {
+        private const val BASIC_AUTH_TOKEN_PREFIX = "Basic"
+    }
+}
---
+++ core/src/main/kotlin/com/inner/circle/core/security/MerchantUserDetails.kt
@@ -0,0 +1,23 @@
+package com.inner.circle.core.security
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class MerchantUserDetails(
+    private val id: String,
+    private val username: String,
+    private val password: String,
+    private val name: String
+) : UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> =
+        mutableListOf(SimpleGrantedAuthority("ROLE_MERCHANT"))
+
+    override fun getUsername(): String = username
+
+    override fun getPassword(): String = password
+
+    fun getId(): String = id
+
+    fun getName(): String = name
+}
