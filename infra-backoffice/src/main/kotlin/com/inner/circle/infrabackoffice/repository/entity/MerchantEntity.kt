beb6843c222b7e7fbcd41b414cd2e12df44c13c8
InvestLee <dlwlgud216@nate.com>
2025-02-11 20:55:43 +0900

    Entity 수정에 따른 비즈니스 로직 수정

--- infra-backoffice/src/main/kotlin/com/inner/circle/infrabackoffice/repository/entity/MerchantEntity.kt
+++ infra-backoffice/src/main/kotlin/com/inner/circle/infrabackoffice/repository/entity/MerchantEntity.kt
@@ -2,8 +2,6 @@ package com.inner.circle.infrabackoffice.repository.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import org.hibernate.annotations.Comment
