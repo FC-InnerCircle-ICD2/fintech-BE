d3c228dcd8f44266c8e16e8e01d4d0514611a453
InvestLee <dlwlgud216@nate.com>
2025-02-11 21:07:39 +0900

    user_card Entity변경

--- infra/src/main/kotlin/com/inner/circle/infra/repository/entity/UserCardEntity.kt
+++ infra/src/main/kotlin/com/inner/circle/infra/repository/entity/UserCardEntity.kt
@@ -1,16 +1,16 @@
 package com.inner.circle.infra.repository.entity
 
+import io.hypersistence.utils.hibernate.id.Tsid
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 
 @Entity
-@Table(name = "userCard")
+@Table(name = "user_card")
 data class UserCardEntity(
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Tsid
     val id: Long,
     @Column(nullable = false)
     val accountId: Long,
