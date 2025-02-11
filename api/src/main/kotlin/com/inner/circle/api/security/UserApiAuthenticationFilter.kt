bf404797ace63970776c01a75ac5d5ea2f7d2589
behappyleee <72903623+behappyleee@users.noreply.github.com>
2025-02-11 22:31:54 +0900

    feat: Account Entity TSID 직접 생성으로 변경

--- api/src/main/kotlin/com/inner/circle/api/security/UserApiAuthenticationFilter.kt
+++ api/src/main/kotlin/com/inner/circle/api/security/UserApiAuthenticationFilter.kt
@@ -1,8 +1,10 @@
 package com.inner.circle.api.security
 
+import com.inner.circle.exception.UserAuthenticationException
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpHeaders
 import org.springframework.web.filter.OncePerRequestFilter
 
 class UserApiAuthenticationFilter : OncePerRequestFilter() {
@@ -11,8 +13,14 @@ class UserApiAuthenticationFilter : OncePerRequestFilter() {
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        println("User Api Filter !!!!")
+        val authHeader =
+            request.getHeader(HttpHeaders.AUTHORIZATION)
+                ?: throw UserAuthenticationException.UnauthorizedException(
+                    "Missing Authorization header"
+                )
 
-        TODO("Not yet implemented")
+
+
+
     }
 }
