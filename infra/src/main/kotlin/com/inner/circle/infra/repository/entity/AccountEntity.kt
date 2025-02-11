b61fdaa3de5df9e3aee1b04b11e75f700c2cd76c
behappyleee <72903623+behappyleee@users.noreply.github.com>
2025-02-11 20:53:31 +0900

    feat: Account Entity 변경

--- infra/src/main/kotlin/com/inner/circle/infra/repository/entity/AccountEntity.kt
+++ infra/src/main/kotlin/com/inner/circle/infra/repository/entity/AccountEntity.kt
@@ -1,20 +1,17 @@
 package com.inner.circle.infra.repository.entity
 
+import io.hypersistence.utils.hibernate.id.Tsid
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
-import java.util.UUID
 
 @Entity
 @Table(name = "account")
 data class AccountEntity(
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Tsid
     val id: Long,
-    @Column(nullable = false, unique = true, length = 36)
     val uuid: UUID,
     @Column(nullable = false, unique = true, length = 255)
     val email: String,
