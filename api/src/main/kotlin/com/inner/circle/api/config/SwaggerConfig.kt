186a5cbe656a4b36ef1cbd0e92996bf702a2ee51
behappyleee <72903623+behappyleee@users.noreply.github.com>
2025-02-12 21:38:15 +0900

    feat: SSE 어노테이션 추가 (#202)
    * feat: SSE 어노테이션 추가

--- api/src/main/kotlin/com/inner/circle/api/config/SwaggerConfig.kt
+++ api/src/main/kotlin/com/inner/circle/api/config/SwaggerConfig.kt
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -34,6 +35,10 @@ class SwaggerConfig(
                 Info()
                     .title("API Documentation")
                     .version("v1")
+            ).servers(
+                listOf(
+                    Server().url(serverUrl).description("HTTPS Server")
+                )
             ).components(
                 Components()
                     .addSecuritySchemes(BASIC_AUTH, basicAuthScheme)
