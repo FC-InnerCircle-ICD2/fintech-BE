92106db50ea054fc4d4be33fccf22b9c66368fa5
behappyleee <72903623+behappyleee@users.noreply.github.com>
2025-02-12 19:36:54 +0900

    feat: class 네이밍 변경

--- api/src/main/kotlin/com/inner/circle/api/security/SecurityConfig.kt
+++ api/src/main/kotlin/com/inner/circle/api/security/SecurityConfig.kt
@@ -1,7 +1,7 @@
 package com.inner.circle.api.security
 
-import com.inner.circle.core.security.MerchantApiKeyProvider
 import com.inner.circle.core.security.AccountValidationProvider
+import com.inner.circle.core.security.MerchantApiKeyProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
