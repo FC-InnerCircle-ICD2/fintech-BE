10998d69c93e75fbdd88d7c8c5cab719e4160449
Raemin Kang <29060626+Raemerrr@users.noreply.github.com>
2025-02-13 21:39:20 +0900

    feat(backoffice): Merchant Entity payment쪽과 통합 (#213)
    * feat(backoffice): Merchant Entity payment쪽과 통합
    
    #211

--- infra-backoffice/src/main/kotlin/com/inner/circle/infrabackoffice/repository/entity/MerchantEntity.kt
+++ infra-backoffice/src/main/kotlin/com/inner/circle/infrabackoffice/repository/entity/MerchantEntity.kt
@@ -2,20 +2,23 @@ package com.inner.circle.infrabackoffice.repository.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
-import org.hibernate.annotations.Comment
 
 @Entity
 @Table(name = "merchant")
 data class MerchantEntity(
     @Id
-    val id: String,
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: String?,
     @Column(nullable = false, unique = true)
-    val mid: String,
-    @Comment("client_secret")
-    @Column(nullable = false, unique = true)
-    var token: String,
+    val username: String,
+    @Column(nullable = false)
+    val password: String,
+    @Column(unique = true)
+    val token: String,
     @Column(nullable = false)
     val name: String
 ) : BaseEntity()
