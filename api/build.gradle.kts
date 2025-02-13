0918c606f4704d383fe59f51a636bd1085fe5f26
gentledot <gentledot.wp@gmail.com>
2025-02-13 22:42:26 +0900

    feat(discord_appender 구성): pr 채널에 오류 메시지가 출력되도록 설정한다.

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     implementation(libs.bundles.springBootValidation)
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.1")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("com.github.napstr:logback-discord-appender:1.0.0")
     testImplementation(libs.bundles.testContainer)
     testImplementation(libs.bundles.kotlinTest)
     testImplementation(libs.bundles.kotlinTestSpring)
