397aa63becdec1412485536d9a51535a08742efd
gentledot <gentledot.wp@gmail.com>
2025-02-15 13:17:37 +0900

    fix(224): sse message type to json
    - payment-in-progress 단계에서 sdk에 필요한 데이터 전달

--- api/src/main/kotlin/com/inner/circle/api/application/PaymentStatusChangedMessageSender.kt
+++ api/src/main/kotlin/com/inner/circle/api/application/PaymentStatusChangedMessageSender.kt
@@ -3,6 +3,8 @@ package com.inner.circle.api.application
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.inner.circle.api.application.dto.PaymentStatusChangedResponse
 import com.inner.circle.api.application.dto.PaymentStatusChangedSsePaymentRequest
+import com.inner.circle.api.application.dto.PaymentStatusEventType
+import com.inner.circle.core.service.dto.ConfirmPaymentCoreDto
 import com.inner.circle.core.sse.SseConnectionPool
 import com.inner.circle.exception.SseException
 import org.slf4j.LoggerFactory
@@ -31,6 +33,34 @@ class PaymentStatusChangedMessageSender(
             val eventData = PaymentStatusChangedResponse.of(eventType, orderId, merchantId)
 
             session.sendMessage(eventType, eventData)
+            log.info(
+                "sse message send. (merchantId: {}, orderId: {}, eventType: {})",
+                merchantId,
+                orderId,
+                eventType
+            )
+        } catch (e: NoSuchElementException) {
+            log.error("get sse session failed", e)
+            throw SseException.connectionNotFound(merchantId, orderId)
+        }
+    }
+
+    fun sendPaymentAuthResultMessage(
+        statusEventType: PaymentStatusEventType,
+        authResult: ConfirmPaymentCoreDto
+    ) {
+        val merchantId = authResult.merchantId
+        val orderId = authResult.orderId
+        val eventType = statusEventType.getEventType()
+
+        try {
+            val uniqueKey = merchantId + "_" + orderId
+            val session =
+                sseConnectionPool.getSession(
+                    uniqueKey
+                )
+
+            session.sendMessage(eventType, authResult)
             log.info(
                 "sse message send. (merchantId: {}, orderId: {}, eventType: {})",
                 merchantId,
