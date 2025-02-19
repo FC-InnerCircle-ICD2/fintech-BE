150f328bb40985624c3036495e5a732298aeff62
youable <youable.framios@gmail.com>
2025-02-19 18:14:34 +0900

    feat: #255 api key 조회 api

--- api-backoffice/src/main/kotlin/com/inner/circle/apibackoffice/controller/ManagementController.kt
+++ api-backoffice/src/main/kotlin/com/inner/circle/apibackoffice/controller/ManagementController.kt
@@ -1,18 +1,22 @@
 package com.inner.circle.apibackoffice.controller
 
 import com.inner.circle.apibackoffice.controller.dto.ApiKeyCreateOrUpdateDto
+import com.inner.circle.apibackoffice.controller.dto.ApiKeyGetDto
 import com.inner.circle.apibackoffice.exception.BackofficeResponse
 import com.inner.circle.corebackoffice.usecase.ApiKeyCreateOrUpdateUseCase
+import com.inner.circle.corebackoffice.usecase.ApiKeyGetUseCase
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
 @Tag(name = "Management", description = "Management API")
 @BackofficeV1Api
 class ManagementController(
-    private val apiKeyCreateOrUpdateUseCase: ApiKeyCreateOrUpdateUseCase
+    private val apiKeyCreateOrUpdateUseCase: ApiKeyCreateOrUpdateUseCase,
+    private val apiKeyGetUseCase: ApiKeyGetUseCase
 ) {
     @Operation(summary = "API 키 발급 및 갱신")
     @PostMapping("/key")
@@ -28,6 +32,14 @@ class ManagementController(
 
     @Operation(summary = "API 키 조회")
     @GetMapping("/key/{id}")
-    fun getKey() {
+    fun getKey(
+        @PathVariable("id") merchantId: Long
+    ): BackofficeResponse<ApiKeyGetDto> {
+        val response = ApiKeyGetDto.of(
+            apiKeyGetUseCase.getApiKeyByMerchantId(
+                ApiKeyGetUseCase.Request(merchantId = merchantId)
+            )
+        )
+        return BackofficeResponse.ok(response)
     }
 }
