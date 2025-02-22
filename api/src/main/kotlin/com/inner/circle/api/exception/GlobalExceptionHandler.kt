953f68e46736dd08ff362346c621838c5b96659b
gentledot <gentledot.wp@gmail.com>
2025-02-22 17:23:08 +0900

    feat(286/mvc-exception-handler): mvc 예외 처리 시 메시지 정제 (#291)
    * feat(2286/mvc-exception-handler): mvc 예외 처리 시 메시지 정제
    
    ResponseEntityExceptionHandler 상속
    
    * feat(2286/mvc-exception-handler): mvc 예외 처리 시 메시지 정제
    
    - 오수정 구간 롤백
    
    * feat(163): exception 메시지 정제
    
    - 영문 -> 한글
    
    * feat(286/mvc-exception-handler): mvc 예외 처리 시 메시지 정제
    
    - backoffice 적용

--- api/src/main/kotlin/com/inner/circle/api/exception/GlobalExceptionHandler.kt
+++ api/src/main/kotlin/com/inner/circle/api/exception/GlobalExceptionHandler.kt
@@ -8,33 +8,24 @@ import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import javax.security.sasl.AuthenticationException
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.HttpMediaTypeNotAcceptableException
+import org.springframework.web.HttpMediaTypeNotSupportedException
+import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import org.springframework.web.servlet.resource.NoResourceFoundException
 
 @RestControllerAdvice
-class GlobalExceptionHandler {
-    private val logger = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
-
-    @ExceptionHandler(MethodArgumentNotValidException::class)
-    fun handleValidationException(
-        ex: MethodArgumentNotValidException
-    ): ResponseEntity<PaymentResponse<Nothing>> =
-        ResponseEntity(
-            PaymentResponse.fail(
-                error =
-                    PaymentError(
-                        code = HttpStatus.BAD_REQUEST.name,
-                        message =
-                            ex.bindingResult.allErrors
-                                .first()
-                                .defaultMessage ?: "유효하지 않은 요청입니다."
-                    )
-            ),
-            HttpStatus.BAD_REQUEST
-        )
+class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
+    private val errorLogger = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
 
     @ExceptionHandler(AppException::class)
     @ApiResponse(
@@ -42,8 +33,8 @@ class GlobalExceptionHandler {
         description = "Unexpected error",
         content = [Content(schema = Schema(implementation = PaymentResponse::class))]
     )
-    fun handleAppException(exception: AppException): ResponseEntity<PaymentResponse<Nothing>> {
-        logger.error("AppException (type = {})", exception::class.simpleName, exception)
+    fun handleAppException(exception: AppException): ResponseEntity<PaymentResponse<Any>> {
+        errorLogger.error("AppException (type = {})", exception::class.simpleName, exception)
 
         var code = exception.status.code
         try {
@@ -71,8 +62,12 @@ class GlobalExceptionHandler {
     )
     fun handleAuthorizationException(
         exception: AuthenticationException
-    ): ResponseEntity<PaymentResponse<Nothing>> {
-        logger.error("AuthenticationException (type = {})", exception::class.simpleName, exception)
+    ): ResponseEntity<PaymentResponse<Any>> {
+        errorLogger.error(
+            "AuthenticationException (type = {})",
+            exception::class.simpleName,
+            exception
+        )
         val unauthorized = com.inner.circle.exception.HttpStatus.UNAUTHORIZED
         return ResponseEntity(
             PaymentResponse.fail(
@@ -86,22 +81,6 @@ class GlobalExceptionHandler {
         )
     }
 
-    @ExceptionHandler(Exception::class)
-    @ApiResponse(
-        responseCode = "500",
-        description = "Unexpected error",
-        content = [Content(schema = Schema(implementation = PaymentResponse::class))]
-    )
-    fun handleException(ex: Exception): ResponseEntity<PaymentResponse<Nothing>> {
-        logger.error("An unexpected error occurred", ex)
-        val status = HttpStatus.INTERNAL_SERVER_ERROR
-        val errorResponse =
-            PaymentResponse.fail(
-                PaymentError(status.toString(), "Unexpected error occurred.")
-            )
-        return ResponseEntity(errorResponse, status)
-    }
-
     @ExceptionHandler(IllegalArgumentException::class)
     @ApiResponse(
         responseCode = "400",
@@ -110,12 +89,141 @@ class GlobalExceptionHandler {
     )
     fun handleIllegalArgumentException(
         ex: IllegalArgumentException
-    ): ResponseEntity<PaymentResponse<Nothing>> {
-        logger.error("IllegalArgumentException (type = {})", ex::class.simpleName, ex)
+    ): ResponseEntity<PaymentResponse<Any>> {
+        errorLogger.error("IllegalArgumentException (type = {})", ex::class.simpleName, ex)
         val status = HttpStatus.BAD_REQUEST
         val errorResponse =
             PaymentResponse.fail(
-                PaymentError(status.toString(), ex.message ?: "Invalid request.")
+                PaymentError(status.toString(), "유효하지 않은 요청입니다.")
+            )
+        return ResponseEntity(errorResponse, status)
+    }
+
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? =
+        ResponseEntity.badRequest().body(
+            PaymentResponse.fail(
+                error =
+                    PaymentError(
+                        code = HttpStatus.BAD_REQUEST.name,
+                        message =
+                            ex.bindingResult.allErrors
+                                .first()
+                                .defaultMessage ?: "유효하지 않은 요청입니다."
+                    )
+            )
+        )
+
+    override fun handleHttpRequestMethodNotSupported(
+        ex: HttpRequestMethodNotSupportedException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        errorLogger.error(
+            "HttpRequestMethodNotSupportedException (type = {})",
+            ex::class.simpleName,
+            ex
+        )
+        val errorResponse =
+            PaymentResponse.fail(
+                PaymentError(
+                    code = com.inner.circle.exception.HttpStatus.METHOD_NOT_ALLOWED.name,
+                    message = "요청하신 주소는 ${ex.method} 를 지원하지 않습니다."
+                )
+            )
+        return ResponseEntity(errorResponse, HttpStatus.METHOD_NOT_ALLOWED)
+    }
+
+    override fun handleHttpMediaTypeNotSupported(
+        ex: HttpMediaTypeNotSupportedException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        errorLogger.error(
+            "HttpMediaTypeNotSupportedException (type = {})",
+            ex::class.simpleName,
+            ex
+        )
+        val errorResponse =
+            PaymentResponse.fail(
+                PaymentError(
+                    code = com.inner.circle.exception.HttpStatus.UNSUPPORTED_MEDIA_TYPE.name,
+                    message = ex.contentType?.let { "지원되지 않는 요청입니다. ($it)" } ?: "지원되지 않는 요청입니다."
+                )
+            )
+        return ResponseEntity(errorResponse, HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+    }
+
+    override fun handleHttpMessageNotReadable(
+        ex: HttpMessageNotReadableException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        errorLogger.error("HttpMessageNotReadableException (type = {})", ex::class.simpleName, ex)
+
+        val errorResponse =
+            PaymentResponse.fail(
+                PaymentError(
+                    code = com.inner.circle.exception.HttpStatus.BAD_REQUEST.name,
+                    message = "유효하지 않은 request 이므로 처리되지 못했습니다."
+                )
+            )
+        return ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST)
+    }
+
+    override fun handleNoResourceFoundException(
+        ex: NoResourceFoundException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        errorLogger.error("NoResourceFoundException (type = {})", ex::class.simpleName, ex)
+        val errorResponse =
+            PaymentResponse.fail(
+                PaymentError(
+                    code = com.inner.circle.exception.HttpStatus.NOT_FOUND.name,
+                    message = "${ex.resourcePath} 는 유효하지 않은 경로입니다."
+                )
+            )
+        return ResponseEntity(errorResponse, HttpStatus.NOT_FOUND)
+    }
+
+    override fun handleHttpMediaTypeNotAcceptable(
+        ex: HttpMediaTypeNotAcceptableException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        errorLogger.error("httpMediaTypeNotAcceptable (type = {})", ex::class.simpleName, ex)
+        val errorResponse =
+            PaymentResponse.fail(
+                PaymentError(
+                    code = com.inner.circle.exception.HttpStatus.BAD_REQUEST.name,
+                    message = "허용되지 않는 요청입니다."
+                )
+            )
+        return ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(Exception::class)
+    @ApiResponse(
+        responseCode = "500",
+        description = "Unexpected error",
+        content = [Content(schema = Schema(implementation = PaymentResponse::class))]
+    )
+    fun handleException(ex: Exception): ResponseEntity<PaymentResponse<Any>> {
+        errorLogger.error("An unexpected error occurred", ex)
+        val status = HttpStatus.INTERNAL_SERVER_ERROR
+        val errorResponse =
+            PaymentResponse.fail(
+                PaymentError(status.toString(), "Unexpected error occurred.")
             )
         return ResponseEntity(errorResponse, status)
     }
