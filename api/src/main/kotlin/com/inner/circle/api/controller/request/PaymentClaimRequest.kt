6d990fd52dfb3deb9631f3b2f3a3e9d9c5eb169c
gentledot <gentledot.wp@gmail.com>
2025-02-22 17:23:42 +0900

    222/request amount limit to 100 million (#292)
    * feat(222): 결제 요청 금액 최대 1억까지 제한
    
    * feat(222): 결제 승인 시 validation

--- api/src/main/kotlin/com/inner/circle/api/controller/request/PaymentClaimRequest.kt
+++ api/src/main/kotlin/com/inner/circle/api/controller/request/PaymentClaimRequest.kt
@@ -1,5 +1,6 @@
 package com.inner.circle.api.controller.request
 
+import jakarta.validation.constraints.DecimalMax
 import jakarta.validation.constraints.DecimalMin
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
@@ -8,6 +9,7 @@ import java.math.BigDecimal
 
 data class PaymentClaimRequest(
     @field:DecimalMin(value = "0.01", message = "금액은 0원 이하로는 입력될 수 없습니다.")
+    @field:DecimalMax(value = "100000000", message = "최대 1억까지 결제를 요청할 수 있습니다.")
     val amount: BigDecimal,
     @field:NotBlank(message = "Order ID는 필수 입력 값입니다.")
     @field:Size(max = 100, message = "Order ID는 최대 100자까지 입력할 수 있습니다.")
