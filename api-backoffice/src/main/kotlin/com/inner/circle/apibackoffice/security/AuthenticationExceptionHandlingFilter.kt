c5b630b9bd94e0e88e4d17bd9f5d75f3e7520689
youable <youable.framios@gmail.com>
2025-02-22 14:09:51 +0900

    fix: 백오피스 에러 log 추가

--- api-backoffice/src/main/kotlin/com/inner/circle/apibackoffice/security/AuthenticationExceptionHandlingFilter.kt
+++ api-backoffice/src/main/kotlin/com/inner/circle/apibackoffice/security/AuthenticationExceptionHandlingFilter.kt
@@ -6,11 +6,13 @@ import com.inner.circle.apibackoffice.exception.BackofficeError
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
 
 class AuthenticationExceptionHandlingFilter : OncePerRequestFilter() {
+    private val log = LoggerFactory.getLogger(AuthenticationExceptionHandlingFilter::class.java)
     private val mapper = ObjectMapper()
 
     override fun doFilterInternal(
@@ -21,6 +23,8 @@ class AuthenticationExceptionHandlingFilter : OncePerRequestFilter() {
         try {
             filterChain.doFilter(request, response)
         } catch (ex: Exception) {
+            log.info("Backoffice Spring Security error : {}", ex.message)
+
             SecurityContextHolder.clearContext()
             val error =
                 BackofficeResponse.fail(
