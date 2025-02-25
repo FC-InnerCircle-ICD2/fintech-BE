3d86bdfaff326e5fa1cf3b1f60e521c42e405e7d
gentledot <gentledot.wp@gmail.com>
2025-02-26 01:58:40 +0900

    fix(infra/cors): cors config 수정
    - MVC CORS 설정을 제거하고 security에서 설정한다.

--- api/src/main/kotlin/com/inner/circle/api/security/SecurityConfig.kt
+++ api/src/main/kotlin/com/inner/circle/api/security/SecurityConfig.kt
@@ -8,6 +8,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.CorsUtils
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -21,7 +26,7 @@ class SecurityConfig(
         http
             .securityMatcher("/api/v1/p/merchant/**")
             .csrf { it.disable() }
-            .cors {}
+            .cors { it.configurationSource(corsConfigurationSource()) }
             .httpBasic { it.disable() }
             .authorizeHttpRequests { authorizeRequests ->
                 authorizeRequests
@@ -41,13 +46,35 @@ class SecurityConfig(
         http
             .securityMatcher("/api/v1/p/user/**")
             .csrf { it.disable() }
+            .cors { it.configurationSource(corsConfigurationSource()) }
             .httpBasic { it.disable() }
             .formLogin { it.disable() }
-            .addFilterBefore(
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers(
+                        CorsUtils::isPreFlightRequest,
+                        AntPathRequestMatcher("/api/v1/p/user/sign-in"),
+                        AntPathRequestMatcher("/api/v1/p/user/sign-up")
+                    ).permitAll()
+                    .anyRequest()
+                    .authenticated()
+            }.addFilterBefore(
                 UserApiAuthenticationFilter(
                     provider = accountValidationProvider,
                     authenticationEntryPoint = authenticationEntryPoint
                 ),
                 UsernamePasswordAuthenticationFilter::class.java
             ).build()
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOriginPatterns = listOf("*")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        configuration.allowedHeaders = listOf("authorization", "content-type")
+        configuration.allowCredentials = true
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/api/v1/p/**", configuration)
+        return source
+    }
 }
