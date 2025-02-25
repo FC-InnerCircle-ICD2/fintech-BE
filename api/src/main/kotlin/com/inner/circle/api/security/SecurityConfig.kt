a4a60a102e899fda0e00badbe2d8e88ff4e6b15d
gentledot <gentledot.wp@gmail.com>
2025-02-25 22:12:57 +0900

    Infra/cors setting (#316) - cors config 추가
    * fix(infra/cors): cors config 추가
    
    * fix(infra/cors): cors config 추가
    
    - cors - configure

--- api/src/main/kotlin/com/inner/circle/api/security/SecurityConfig.kt
+++ api/src/main/kotlin/com/inner/circle/api/security/SecurityConfig.kt
@@ -8,6 +8,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -22,7 +25,7 @@ class SecurityConfig(
             .securityMatcher("/api/v1/p/merchant/**")
             .csrf { it.disable() }
             .httpBasic { it.disable() }
-            .cors { it.disable() }
+            .cors { it.configurationSource(corsConfigurationSource()) }
             .authorizeHttpRequests { authorizeRequests ->
                 authorizeRequests
                     .anyRequest()
@@ -41,7 +44,7 @@ class SecurityConfig(
         http
             .securityMatcher("/api/v1/p/user/**")
             .csrf { it.disable() }
-            .cors { it.disable() }
+            .cors { it.configurationSource(corsConfigurationSource()) }
             .httpBasic { it.disable() }
             .formLogin { it.disable() }
             .addFilterBefore(
@@ -51,4 +54,16 @@ class SecurityConfig(
                 ),
                 UsernamePasswordAuthenticationFilter::class.java
             ).build()
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("*")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        configuration.allowedHeaders = listOf("authorization", "content-type")
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
 }
