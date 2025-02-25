ab9ea8d3203f294880804966c7d02f74a85913a4
gentledot <gentledot.wp@gmail.com>
2025-02-26 01:58:40 +0900

    fix(infra/cors): cors config 수정
    - MVC CORS 설정을 제거하고 security에서 설정한다.

--- api/src/main/kotlin/com/inner/circle/api/security/SecurityConfig.kt
+++ api/src/main/kotlin/com/inner/circle/api/security/SecurityConfig.kt
@@ -11,6 +11,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.CorsUtils
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -48,6 +53,16 @@ class SecurityConfig(
             .cors { it.configurationSource(corsConfigurationSource()) }
             .httpBasic { it.disable() }
             .formLogin { it.disable() }
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers(
+                        CorsUtils::isPreFlightRequest,
+                        AntPathRequestMatcher("/api/v1/p/user/sign-in"),
+                        AntPathRequestMatcher("/api/v1/p/user/sign-up")
+                    ).permitAll()
+                    .anyRequest()
+                    .authenticated()
+            }
             .addFilterBefore(
                 UserApiAuthenticationFilter(
                     provider = accountValidationProvider,
