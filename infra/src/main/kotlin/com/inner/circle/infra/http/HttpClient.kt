3b3a351e4b182e71b07d39cb70ce7da47e49228f
InvestLee <dlwlgud216@nate.com>
2025-02-26 21:52:05 +0900

    네트워크 이슈 발생 시 재시도 수행

--- infra/src/main/kotlin/com/inner/circle/infra/http/HttpClient.kt
+++ infra/src/main/kotlin/com/inner/circle/infra/http/HttpClient.kt
@@ -9,6 +9,7 @@ import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import org.slf4j.LoggerFactory
 import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Recover
 import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
 import retrofit2.Call
@@ -21,65 +22,74 @@ import retrofit2.http.Url
 
 @Component
 class HttpClient {
-    companion object {
-        private val log = LoggerFactory.getLogger(HttpClient::class.java)
-        private val gson = Gson()
+    private val log = LoggerFactory.getLogger(HttpClient::class.java)
+    private val gson = Gson()
+    private var attempt = 0
 
-        @Retryable(
-            value = [IOException::class],
-            maxAttempts = 3,
-            backoff = Backoff(delay = 1000)
-        )
-        fun sendPostRequest(
-            baseUrl: String,
-            endpoint: String,
-            params: Map<String, Any>
-        ): Map<String, Any> {
-            val retrofit =
-                Retrofit
-                    .Builder()
-                    .baseUrl(baseUrl)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build()
+    @Retryable(
+        value = [IOException::class],
+        maxAttempts = 3,
+        backoff = Backoff(delay = 1000)
+    )
+    fun sendPostRequest(
+        baseUrl: String,
+        endpoint: String,
+        params: Map<String, Any>
+    ): Map<String, Any> {
+        val retrofit =
+            Retrofit
+                .Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
 
-            val jsonParams = gson.toJson(params)
-            val mediaType = MediaType.parse("application/json; charset=utf-8")
-            val body = RequestBody.create(mediaType, jsonParams)
+        val jsonParams = gson.toJson(params)
+        val mediaType = MediaType.parse("application/json; charset=utf-8")
+        val body = RequestBody.create(mediaType, jsonParams)
 
-            val apiService = retrofit.create(ApiService::class.java)
-            val call: Call<ResponseBody> = apiService.sendPostRequest(endpoint, body)
+        val apiService = retrofit.create(ApiService::class.java)
+        val call: Call<ResponseBody> = apiService.sendPostRequest(endpoint, body)
 
-            return executeCall(call)
-        }
+        return executeCall(call)
+    }
 
-        private fun executeCall(call: Call<ResponseBody>): Map<String, Any> {
-            try {
-                val response: Response<ResponseBody> = call.execute()
+    fun executeCall(call: Call<ResponseBody>): Map<String, Any> {
+        attempt++
+        log.info("재시도 횟수 : ${attempt}")
+        try {
+            val response: Response<ResponseBody> = call.execute()
 
-                if (response.isSuccessful) {
-                    // 응답 본문을 동적으로 파싱하여 바로 반환
-                    val responseBody = response.body()?.string()
-                    responseBody?.let {
-                        // JSON 응답을 Map으로 파싱
-                        return gson.fromJson(it, Map::class.java) as Map<String, Any>
-                    } ?: throw NullPointerException("Response body is null")
-                }
-                throw CardCompanyException.ConnenctException(
-                    code = response.code(),
-                    msg = response.message()
-                )
-            } catch (e: IOException) {
-                log.error("[ERROR] 네트워크 요청 실패: ${e.message}")
-                throw PaymentException.CardAuthFailException()
+
+            if (response.isSuccessful) {
+                // 응답 본문을 동적으로 파싱하여 바로 반환
+                val responseBody = response.body()?.string()
+                responseBody?.let {
+                    // JSON 응답을 Map으로 파싱
+                    return gson.fromJson(it, Map::class.java) as Map<String, Any>
+                } ?: throw NullPointerException("Response body is null")
             }
+            throw CardCompanyException.ConnenctException(
+                code = response.code(),
+                msg = response.message()
+            )
+        } catch (e: IOException) {
+            log.error("[ERROR] 네트워크 요청 실패: ${e.message}")
+            throw e
         }
+    }
 
-        fun interface ApiService {
-            @POST
-            fun sendPostRequest(
-                @Url url: String,
-                @Body body: RequestBody
-            ): Call<ResponseBody>
-        }
+    @Recover
+    fun recover(e: IOException, baseUrl: String, endpoint: String, params: Map<String, Any>): Map<String, Any> {
+        log.error("최대 재시도 횟수 초과. 요청을 다시 시도했지만 실패했습니다.")
+        attempt = 0
+        throw PaymentException.CardAuthFailException()
+    }
+
+    fun interface ApiService {
+        @POST
+        fun sendPostRequest(
+            @Url url: String,
+            @Body body: RequestBody
+        ): Call<ResponseBody>
     }
 }
